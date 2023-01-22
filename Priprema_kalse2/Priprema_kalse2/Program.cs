49ebadbb1d694b33551bc30243634cbf854bd789
aleino0 <113008053+aleino0@users.noreply.github.com>
2023-01-22 19:48:57 +0100

    Add files via upload

---
+++ Priprema_kalse2/Priprema_kalse2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema_kalse2
+{
+    class GeometrijskiLik
+    {
+        public int brojStranica;
+        double[] stranice = new double[10];
+
+        public void UcitajStranice()
+        {
+            for (int i = 0; i < brojStranica; i++)
+            {
+                Console.WriteLine("Upišite duljinu {0}. stranice", i + 1);
+                string duljina = Console.ReadLine();
+                stranice[i] = Convert.ToDouble(duljina);
+            }
+        }
+
+        public double Opseg()
+        {
+            double opseg=0;
+            for (int i = 0; i < brojStranica; i++)
+            {
+                opseg += stranice[i];
+            }
+            return opseg;
+        } 
+    }
+
+    class Trokut : GeometrijskiLik
+    {
+        public Trokut()
+        {
+            brojStranica = 3;
+        }
+    }
+
+    class Cetverokut : GeometrijskiLik 
+    {
+        public Cetverokut() 
+        {
+            brojStranica = 4;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Trokut trokut = new Trokut();
+            trokut.UcitajStranice();
+            Console.WriteLine("Opseg trokuta je {0}", trokut.Opseg());
+            Cetverokut cetverokut = new Cetverokut();
+            cetverokut.UcitajStranice();
+            Console.WriteLine("Opseg četverokuta je {0}", cetverokut.Opseg());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Priprema_klase1/Priprema_klase1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priprema_klase1
+{
+    class Fruit
+    {
+        private string vrsta;
+        private string boja;
+        private bool jeLiSlatko;
+
+        public Fruit(string vrsta)
+        {
+            this.vrsta = vrsta;
+        }
+        public Fruit(string vrsta, string boja, bool jeLiSlatko)
+        {
+            this.vrsta = vrsta;
+            this.boja = boja;
+            this.jeLiSlatko = jeLiSlatko;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit x = new Fruit("Jagoda", "Crvena", true);
+            Fruit y = new Fruit("Limun", "Žuta", false);
+            Fruit z = new Fruit("Avokado");
+
+            Console.ReadKey();
+        }
+    }
+}
